5df819c3258151d833e4f7ce32519a589c1090f9
Sunny <sraj1333.sr@gmail.com>
2025-05-11 23:19:56 +0530

    Practice-Day-11

---
+++ CodeChef/Basic-Math/chef-and-chocolates.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y, z;
+        cin >> x >> y >> z;
+        // your code goes here
+        cout << (((x * 5) + (y * 10)) / z) << endl;
+    }
+}
