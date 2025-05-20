8400e22182d2c505e6614dab6ca50d653b4dcefb
Sunny <sraj1333.sr@gmail.com>
2025-05-21 02:32:52 +0530

    Practice-Day-20

---
+++ LeetCode/Arrays/1550.three-consecutive-odds.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=1550 lang=cpp
+ *
+ * [1550] Three Consecutive Odds
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool threeConsecutiveOdds(vector<int> &arr)
+    {
+        int count = 0;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            if (arr[i] % 2 == 1)
+            {
+                count++;
+            }
+            else
+            {
+                count = 0;
+            }
+
+            if (count == 3)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+// @lc code=end
